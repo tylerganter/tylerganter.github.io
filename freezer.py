f594e5f1d92a474e3f00f31b1b614d24207d2e84
Tyler Ganter <tyler.h.ganter@gmail.com>
2018-11-22 13:50:38 +0100

    added cleaning option

--- freezer.py
+++ freezer.py
@@ -1,4 +1,13 @@
+import sys, os
 from _website.main import freezer
 
 if __name__ == '__main__':
+    if '--clean' in sys.argv[1::]:
+        """Delete the 'cached' files in static"""
+        repo_path = os.path.abspath(os.path.dirname(__file__))
+        static_path = os.path.join(repo_path, 'static')
+
+        import subprocess
+        subprocess.call(['rm', '-rf', static_path])
+
     freezer.freeze()
