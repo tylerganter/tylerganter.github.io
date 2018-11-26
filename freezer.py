b424396e2135b3570d46403e0592f27f2df920b5
Tyler Ganter <tyler.h.ganter@gmail.com>
2018-11-26 14:58:59 +0100

    removing projects tab for now

--- freezer.py
+++ freezer.py
@@ -1,7 +1,20 @@
 import sys, os
+import argparse
 from _website.main import freezer
 
 if __name__ == '__main__':
+    # # TODO replace with this
+    # parser = argparse.ArgumentParser()
+    # parser.add_argument(
+    #     '--clean',
+    #     help = 'Delete the "cached" files in static',
+    #     required = False
+    # )
+    # ## parse all arguments
+    # args = parser.parse_args()
+    # arguments = args.__dict__
+    # clean = arguments.pop('clean')
+
     if '--clean' in sys.argv[1::]:
         """Delete the 'cached' files in static"""
         repo_path = os.path.abspath(os.path.dirname(__file__))
