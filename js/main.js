d652adccd833152fb592b71c6f9853acbf7a5113
Tyler Ganter <tyler@voxel51.com>
2019-01-20 15:47:38 -0500

    adaptive height

--- js/main.js
+++ js/main.js
@@ -3,7 +3,19 @@ onLoad = function() {
 	// $("#about-me").text("This was set with jQuery!");
 	// $("#about-me").html("<h1>This is jQuery that overwrote the HTML</h1>");
 
+	setIntroHeight();
+
 	swipeSquiggles();
+}
+
+
+// set the height to align with the size of the browser window
+function setIntroHeight() {
+	var windowHeight = window.innerHeight 
+					   || document.documentElement.clientHeight
+					   || document.body.clientHeight;
+
+	$("#intro").height(windowHeight - $("header").height());
 }
 
 
