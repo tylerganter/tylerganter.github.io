eb53dae09b373a21779454642eaac6563b343fa1
Tyler Ganter <tyler@voxel51.com>
2019-01-20 11:33:13 -0500

    deleting flask content

--- run.py
+++
@@ -1,7 +0,0 @@
-import os
-from _website.main import app
-
-if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
-    # main.app.run(host='0.0.0.0', port=port)
-    app.run(debug=True)
